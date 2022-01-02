7dc5c366f7da7e4d62116f5355e068ba8371c8ff
Sahil Singh Fartyal <57360832+Sahil4405@users.noreply.github.com>
2022-01-02 14:31:28 +0530

    Add files via upload

---
+++ Goldman Sachs/2.Overlapping_Rectangles.cpp
@@ -0,0 +1,17 @@
+
+// Questions Link: https://practice.geeksforgeeks.org/problems/overlapping-rectangles1924/1/#
+
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        // code here
+        if(L1[0] > R2[0] || L2[0] > R1[0])
+        return false;
+        
+        if(L1[1] < R2[1] || L2[1] < R1[1])
+        return false;
+        
+        
+        return true;
+    }
+};
